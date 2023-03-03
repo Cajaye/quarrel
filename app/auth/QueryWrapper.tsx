3e94c1ac26efdb65c7047e868a567efa795689aa
Ca-Jaye Clarke <cajayeclarke44@gmail.come>
2023-03-02 22:08:45 -0500

    toaster setup

--- app/auth/QueryWrapper.tsx
+++ app/auth/QueryWrapper.tsx
@@ -3,6 +3,8 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactNode } from "react"
 
+import { Toaster } from 'react-hot-toast';
+
 const queryClient = new QueryClient()
 
 type Props = {
@@ -11,6 +13,7 @@ type Props = {
 
 export default function QueryWrapper({ children }: Props) {
     return <QueryClientProvider client={queryClient}>
+        <Toaster/>
         {children}
     </QueryClientProvider>
 }
