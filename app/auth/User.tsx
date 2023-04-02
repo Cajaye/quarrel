a765c557b448532ad168714d5ccebc8068967051
Ca-Jaye Clarke <cajayeclarke44@gmail.come>
2023-04-02 15:19:26 -0500

    remove next link

--- app/auth/User.tsx
+++ app/auth/User.tsx
@@ -1,25 +1,23 @@
-'use client'
-import Image from "next/image"
-import Link from "next/link"
+"use client";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
-    avatar: string,
-    name:string
-}
+  avatar: string;
+  name: string;
+};
 
-export default function User({avatar, name}:Props) { 
-    return (
-        <div className="flex items-center gap-4">
-            <Image
-                className="rounded-full"
-                src={avatar}
-                alt="avatar"
-                width={32}
-                height={32}
-            />
-            <Link href={"/profile"}>
-                <h3 className="text-gray-700">{name}</h3>
-            </Link>
-        </div>
-    )
+export default function User({ avatar, name }: Props) {
+  return (
+    <div className="flex items-center gap-4">
+      <Image
+        className="rounded-full"
+        src={avatar}
+        alt="avatar"
+        width={32}
+        height={32}
+          />
+        <h3 className="text-gray-700">{name}</h3>
+    </div>
+  );
 }
